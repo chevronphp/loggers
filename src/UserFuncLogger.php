ccb54c728379937b894cdda73783bd1b130de75a
henderjon <jhenderson@capstonedigital.com>
2014-07-11 15:17:36 -0500

    adds nebulous UserFuncLogger

---
+++ src/UserFuncLogger.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Chevron\Loggers;
+
+use \Psr\Log\AbstractLogger;
+
+class UserFuncLogger extends AbstractLogger {
+
+	protected $output;
+
+	public function __construct(callable $func){
+		$this->output = $func;
+	}
+
+	public function log( $level, $message, array $context = array() ) {
+		return call_user_func($this->output, $level, $message, $context);
+	}
+
+}
