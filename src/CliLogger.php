ad66263df695d9adc117758e80c5b19b0f9892b6
henderjon <jhenderson@capstonedigital.com>
2014-08-22 20:56:48 -0500

    No reason to extend UserFunc ...

--- src/CliLogger.php
+++ src/CliLogger.php
@@ -2,30 +2,27 @@
 
 namespace Chevron\Loggers;
 
-class CliLogger extends UserFuncLogger {
+use \Psr\Log\AbstractLogger;
 
-	protected $func;
+class CliLogger extends AbstractLogger {
 
-	public function __construct(){
-		$this->func = function($level, $message, array $context){
+	public function log($level, $message, array $context = []){
+		$context = ["level" => strtoupper($level), "message" => $message, "timestamp" => date("c")] + $context;
 
-			$context = ["level" => strtoupper($level), "message" => $message, "timestamp" => date("c")] + $context;
+		$len = 0;
+		foreach($context as $key => $value){
+			if( ($l = strlen($key)) > $len){ $len = $l; }
+		}
 
-			$len = 0;
-			foreach($context as $key => $value){
-				if( ($l = strlen($key)) > $len){ $len = $l; }
-			}
+		$output = "\n\n-------------------------\n\n";
 
-			$output = "\n\n-------------------------\n\n";
+		foreach($context as $key => $value){
+			$output .= sprintf("%{$len}s => %s\n", $key, $value);
+		}
 
-			foreach($context as $key => $value){
-				$output .= sprintf("%{$len}s => %s\n", $key, $value);
-			}
+		$output .= "\n\n-------------------------\n\n";
 
-			$output .= "\n\n-------------------------\n\n";
-
-			echo $output;
-		};
+		echo $output;
 	}
 
 }
